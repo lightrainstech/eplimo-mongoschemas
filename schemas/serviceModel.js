f5702ee2b4e98a2b69928bd9a629260941a7ed7b
sruthi23 <sruthi@lightrains.com>
2022-10-14 18:52:22 +0530

    Add service model

---
+++ schemas/serviceModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const ServiceSchema = new mongoose.Schema(
+  {
+    user: { type: Schema.ObjectId, ref: 'User', required: true },
+    service: {
+      type: String
+    },
+    price: {
+      type: String,
+      default: '0'
+    },
+    onSale: {
+      type: Boolean,
+      default: false
+    },
+    isPromoted: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+)
+
+ServiceSchema.methods = {
+  getServices: async () => {
+    const Service = mongoose.model('Service')
+    return await Service.find({})
+  },
+  getServicesByuser: async () => {
+    const Service = mongoose.model('Service')
+    return await Service.find({ user })
+  }
+}
+
+ServiceSchema.index({ user: 1 })
+
+module.exports = mongoose.model('Service', ServiceSchema)
