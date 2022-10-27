549263bafc4cad5f152deb8ca879748115ab4415
sruthi23 <sruthi@lightrains.com>
2022-10-27 11:23:32 +0530

    Add new collection for app login

---
+++ schemas/appLoginModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema
+
+const appLoginSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    dateIndex: {
+      type: String
+    },
+    isProcessed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+)
+
+appLoginSchema.methods = {
+  getByEmail: async function (email) {
+    const AppLogin = mongoose.model('AppLogin')
+    return AppLogin.find({
+      email
+    }).limit(1)
+  },
+  updateStatus: async function (email) {
+    const AppLogin = mongoose.model('AppLogin')
+    return AppLogin.findOneAndUpdate(
+      {
+        email
+      },
+      { $set: { isProcessed: true } },
+      { new: true }
+    ).limit(1)
+  }
+}
+appLoginSchema.index({
+  email: 1
+})
+
+module.exports = mongoose.model('AppLogin', appLoginSchema)
