425a4002411f78105d2c42329feff6c7c7b90b6d
sruthi23 <sruthi@lightrains.com>
2023-06-23 11:46:33 +0530

    Add schema for testimonials

--- schemas/partnerverse/testimonialModel.js
+++ schemas/partnerverse/testimonialModel.js
@@ -7,9 +7,24 @@ const TestimonialSchema = new mongoose.Schema(
       type: String,
       required: true
     },
-    user: { type: ObjectId, ref: 'User', required: true }
+    user: { type: ObjectId, ref: 'User' },
+    name: {
+      type: String,
+      required: true
+    }
   },
   { timestamps: true }
 )
 
+TestimonialSchema.methods = {
+  getAllTestimonials: async function () {
+    try {
+      const TestimonialModel = mongoose.model('Testimonial')
+      let data = await TestimonialModel.find({})
+      return data
+    } catch (error) {
+      throw error
+    }
+  }
+}
 module.exports = mongoose.model('Testimonial', TestimonialSchema)
