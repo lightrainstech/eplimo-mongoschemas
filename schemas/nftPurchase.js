2a0f17b3081d2440c0dee81b9a5eb98d4b19969b
sruthi23 <sruthi@lightrains.com>
2023-08-21 16:13:32 +0530

    Add collection for NFT purchases

---
+++ schemas/nftPurchase.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema
+const Schema = mongoose.Schema
+
+const NftPurchaseSchema = new mongoose.Schema(
+  {
+    user: { type: Schema.ObjectId, ref: 'User', required: true },
+    referralCode: {
+      type: String
+    },
+    nft: {
+      type: Schema.ObjectId,
+      ref: 'Asset',
+      required: true
+    },
+    txnHash: {
+      type: String,
+      unique: true,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+NftPurchaseSchema.methods = {}
+
+module.exports = mongoose.model('NftPurchase', NftPurchaseSchema)
