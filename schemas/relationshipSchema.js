8d2263cc1166929cc343418f2722e36ba75bb06a
sruthi23 <sruthi@lightrains.com>
2023-09-18 10:42:45 +0530

    Add function to get user by levels and their sales

--- schemas/relationshipSchema.js
+++ schemas/relationshipSchema.js
@@ -69,6 +69,142 @@ RelationshipSchema.methods = {
     } catch (error) {
       throw error
     }
+  },
+  getFirstLevelDescendants: async function (user) {
+    try {
+      const RelationshipModel = mongoose.model('RelationshipSchema')
+      return await RelationshipModel.aggregate([
+        {
+          $match: { parent: user } // Replace 'parentUserId' with the actual parent's user ID
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'child',
+            foreignField: '_id',
+            as: 'descendant'
+          }
+        },
+        {
+          $unwind: '$descendant'
+        },
+        {
+          $lookup: {
+            from: 'nftpurchases',
+            let: { referralCode: '$descendant.referalCode' },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ['$referralCode', '$$referralCode']
+                  }
+                }
+              },
+              {
+                $lookup: {
+                  from: 'assets',
+                  localField: 'nft',
+                  foreignField: '_id',
+                  as: 'nftDetails'
+                }
+              },
+              {
+                $group: {
+                  _id: null,
+                  totalNFTPrice: {
+                    $sum: {
+                      $toDouble: { $arrayElemAt: ['$nftDetails.price', 0] }
+                    }
+                  }
+                }
+              }
+            ],
+            as: 'purchaseSummary'
+          }
+        },
+        {
+          $lookup: {
+            from: 'directstakes',
+            localField: 'descendant.referalCode',
+            foreignField: 'referralCode',
+            as: 'stakesSummary'
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            userId: '$descendant._id',
+            name: '$descendant.name',
+            email: '$descendant.email',
+            totalNftSale: {
+              $arrayElemAt: ['$purchaseSummary.totalNFTPrice', 0]
+            },
+            totalStakes: { $sum: '$stakesSummary.stake' },
+            createdAt: 1
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            nftSalesByLevel: { $sum: '$totalNftSale' },
+            stakesByLevel: { $sum: '$totalStakes' },
+            descendants: {
+              $push: {
+                userId: '$userId',
+                name: '$name',
+                email: '$email',
+                totalNftSale: '$totalNftSale',
+                totalStakes: '$totalStakes',
+                createdAt: '$createdAt'
+              }
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            nftSalesByLevel: 1,
+            stakesByLevel: 1,
+            descendants: 1
+          }
+        }
+      ])
+    } catch (error) {
+      throw error
+    }
+  },
+  getTotalLevel: async function (user) {
+    const RelationshipModel = mongoose.model('RelationshipSchema')
+    return await RelationshipModel.aggregate([
+      {
+        $match: { parent: user } // Replace 'parentUserId' with the actual parent's user ID
+      },
+      {
+        $graphLookup: {
+          from: 'relationshipschemas',
+          startWith: '$child',
+          connectFromField: 'child',
+          connectToField: 'parent',
+          //maxDepth: 10, // Adjust this value based on your maximum expected depth
+          as: 'descendants'
+        }
+      },
+      {
+        $unwind: '$descendants'
+      },
+      {
+        $group: {
+          _id: null,
+          maxLevel: { $max: '$descendants.level' }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          numberOfLevels: { $add: ['$maxLevel', 1] } // Add 1 to the max level to count the top-level parent as well
+        }
+      }
+    ])
   }
 }
 
