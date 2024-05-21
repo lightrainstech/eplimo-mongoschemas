2d6c3218f8d794fd22e0b79b880ab0a5c761d211
sruthi23 <sruthi@lightrains.com>
2024-05-21 20:26:50 +0530

    Add function to save and retrieve data

--- schemas/healthInfoModel.js
+++ schemas/healthInfoModel.js
@@ -11,7 +11,10 @@ const HealthInfoSchema = new mongoose.Schema(
     dataType: {
       type: String
     },
-    date: {
+    startTime: {
+      type: Date
+    },
+    endTime: {
       type: Date
     },
     data: {
@@ -19,9 +22,82 @@ const HealthInfoSchema = new mongoose.Schema(
     },
     provider: {
       type: String
+    },
+    dataProviderId: {
+      type: String
     }
   },
   { timestamps: true }
 )
+
+HealthInfoSchema.methods = {
+  saveData: async function (data) {
+    try {
+      const HealthInfo = mongoose.model('HealthInfo')
+      return await HealthInfo.findOneAndUpdate(
+        {
+          user: ObjectId(data.user),
+          dataType: data.type,
+          startTime: { $eq: data.startTime },
+          endTime: { $eq: data.endTime }
+        },
+        {
+          $set: {
+            data: data.data,
+            provider: data.provider,
+            startTime: data.startTime,
+            endTime: data.endTime,
+            dataProviderId: data.dataProviderId
+          }
+        },
+        {
+          new: true,
+          upsert: true
+        }
+      )
+    } catch (error) {
+      throw error
+    }
+  },
+  getHealthInfo: async function (userId, date) {
+    try {
+      console.log(userId, date)
+      const HealthInfo = mongoose.model('HealthInfo')
+      return await HealthInfo.aggregate([
+        {
+          $match: {
+            user: ObjectId(userId),
+            startTime: { $gte: date }
+          }
+        },
+        {
+          $unwind: '$data'
+        },
+        {
+          $sort: { 'data.endTime': -1 }
+        },
+        {
+          $group: {
+            _id: '$dataType',
+            documents: { $first: '$data' },
+            user: { $first: '$user' },
+            dataProviderId: { $first: '$dataProviderId' }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            dataType: '$_id',
+            documents: 1,
+            user: 1,
+            dataProviderId: 1
+          }
+        }
+      ])
+    } catch (error) {
+      throw error
+    }
+  }
+}
 
 module.exports = mongoose.model('HealthInfo', HealthInfoSchema)
