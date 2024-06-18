eb5ccc153c8fde861f450f22168de34af32288e3
sruthi23 <sruthi@lightrains.com>
2024-06-18 11:58:19 +0530

    Add function to record txns

--- schemas/lftOrders.js
+++ schemas/lftOrders.js
@@ -7,6 +7,9 @@ const LftPurchaseOrderSchema = new mongoose.Schema(
     wallet: {
       type: String
     },
+    amount: {
+      type: Number
+    },
     referralCode: {
       type: String
     },
@@ -14,23 +17,77 @@ const LftPurchaseOrderSchema = new mongoose.Schema(
       type: String,
       unique: true
     },
-    status: {
+    paymentTxnHash: {
+      type: String,
+      default: null
+    },
+    paymentStatus: {
       type: String,
       default: 'pending'
     },
     assetId: {
       type: Number,
       default: null
+    },
+    transferred: {
+      type: Boolean,
+      default: false
+    },
+    assetTxnHash: {
+      type: String,
+      default: null
     }
   },
   { timestamps: true }
 )
+
+LftPurchaseOrderSchema.methods = {
+  updateStatus: async function (args) {
+    try {
+      const { orderId, hash, assetId } = args
+      const LftPurchaseOrderModel = mongoose.model('LftPurchaseOrder')
+      return await LftPurchaseOrderModel.findOneAndUpdate(
+        { orderId },
+        {
+          $set: {
+            paymentTxnHash: hash,
+            assetId: assetId,
+            paymentStatus: 'completed'
+          }
+        },
+        { new: true }
+      )
+    } catch (error) {
+      throw error
+    }
+  },
+  fetchLatestAsset: async function () {
+    try {
+      const LftPurchaseOrderModel = mongoose.model('LftPurchaseOrder')
+      return await LftPurchaseOrderModel.findOne({
+        paymentStatus: 'completed'
+      }).sort({ assetId: -1 })
+    } catch (error) {
+      throw error
+    }
+  }
+}
 
 LftPurchaseOrderSchema.index(
   { assetId: 1 },
   {
     unique: true,
     partialFilterExpression: { assetId: { $type: 'number' } }
+  },
+  { assetTxnHash: 1 },
+  {
+    unique: true,
+    partialFilterExpression: { assetTxnHash: { $type: 'string' } }
+  },
+  { paymentTxnHash: 1 },
+  {
+    unique: true,
+    partialFilterExpression: { paymentTxnHash: { $type: 'string' } }
   }
 )
 
