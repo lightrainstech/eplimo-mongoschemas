ee5d18e548e6258429a1b664ca7650144b040829
sruthi23 <sruthi@lightrains.com>
2024-07-03 10:24:44 +0530

    Add api to fetch scan history

--- schemas/binahVitals.js
+++ schemas/binahVitals.js
@@ -18,4 +18,22 @@ const BinahVitalsSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
+BinahVitalsSchema.methods = {
+  getScanHistory: async function (page, user) {
+    try {
+      const ScanHistory = mongoose.model('BinahVitals')
+      let limit = 18
+      page = page === 0 ? 0 : page - 1
+      return await ScanHistory.find({
+        user
+      })
+        .sort({ createdAt: -1 })
+        .limit(limit)
+        .skip(limit * page)
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
 module.exports = mongoose.model('BinahVitals', BinahVitalsSchema)
